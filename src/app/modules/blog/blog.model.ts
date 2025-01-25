d4f2d375762fe89239f069da4df4b09eb52fd5a6
manik <maniksarker652@gmail.com>
2025-01-25 14:21:26 +0600

    make changes in blog model

--- src/app/modules/blog/blog.model.ts
+++ src/app/modules/blog/blog.model.ts
@@ -1,12 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
 import { IBlog } from './blog.interface';
 
-const BlogSchema: Schema = new Schema({
-  title: { type: String, required: true, trim: true },
-  hashtag: { type: String, required: true, trim: true },
-  blog_image: { type: String, required: true },
-  description: { type: String, required: true, trim: true },
-});
+const BlogSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    hashtag: { type: String, required: true, trim: true },
+    blog_image: { type: String, required: true },
+    description: { type: String, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 // Create and export the Blog model
 const Blog = mongoose.model<IBlog>('Blog', BlogSchema);

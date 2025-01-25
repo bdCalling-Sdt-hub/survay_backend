eed7a3232b59eb7363255c2959cd7f82ea0ad751
manik <maniksarker652@gmail.com>
2025-01-25 14:31:34 +0600

    create services for story management

---
+++ src/app/modules/story/story.service.ts
@@ -0,0 +1,72 @@
+import httpStatus from 'http-status';
+import AppError from '../../error/appError';
+import QueryBuilder from '../../builder/QueryBuilder';
+import unlinkFile from '../../helper/unLinkFile';
+import { IStory } from './story.interface';
+import Story from './story.model';
+
+const createStory = async (profileId: string, payload: IStory) => {
+  const result = await Story.create({ ...payload, author: profileId });
+  return result;
+};
+
+const updateStory = async (id: string, payload: Partial<IStory>) => {
+  const story = await Story.findById(id);
+  if (!story) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Story not found');
+  }
+  const result = await Story.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (payload.story_image) {
+    unlinkFile(payload.story_image);
+  }
+  return result;
+};
+
+const getAllStory = async (query: Record<string, unknown>) => {
+  const storyQuery = new QueryBuilder(
+    Story.find().populate({ path: 'author', select: 'name profile_image' }),
+    query,
+  )
+    .search(['title', 'description'])
+    .fields()
+    .filter()
+    .paginate()
+    .sort();
+
+  const result = await storyQuery.modelQuery;
+  const meta = await storyQuery.countTotal();
+  return {
+    meta,
+    result,
+  };
+};
+const getSingleStory = async (id: string) => {
+  const result = await Story.findById(id);
+  return result;
+};
+
+const deleteSingleStory = async (profileId: string, id: string) => {
+  const story = await Story.findOne({ author: profileId, _id: id });
+  if (!story) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Blog not found');
+  }
+  const result = await Story.findByIdAndDelete(id);
+  if (story.story_image) {
+    unlinkFile(story.story_image);
+  }
+  return result;
+};
+
+const BlogService = {
+  createStory,
+  updateStory,
+  getAllStory,
+  getSingleStory,
+  deleteSingleStory,
+};
+
+export default BlogService;
